b79c48e338415826db3dc910ece0bb3987669210
audy <audyliaaskawidiaputri@gmail.com>
2026-07-19 20:23:15 +0700

    Update tugas1

--- tugas1.dart
+++ tugas1.dart
@@ -1,25 +1,38 @@
 void main() {
+  // Memanggil fungsi untuk menampilkan biodata
+  tampilkanBiodata();
+}
+
+// Fungsi untuk menampilkan seluruh biodata anggota
+void tampilkanBiodata() {
+  // Data anggota
   String nama = "Audylia Aska Widiaputri";
   int umur = 20;
-  double tinggi = 166.5;
+  double tinggiBadan = 166.5;
   bool statusAktif = true;
 
+  // Daftar buku favorit
   List<String> bukuFavorit = [
     "The Strangers",
     "The Metamorphosis",
     "No Longer Human"
   ];
 
+  /*
+    Informasi tambahan
+    Disimpan dalam bentuk Map
+  */
   Map<String, dynamic> informasi = {
     "Alamat": "Jakarta",
     "Profesi": "Mahasiswa"
   };
 
-  print("Nama : $nama");
-  print("Umur : $umur");
-  print("Tinggi : $tinggi cm");
-  print("Status Aktif : $statusAktif");
-  print("Buku Favorit : $bukuFavorit");
-  print("Alamat : ${informasi["Alamat"]}");
-  print("Profesi : ${informasi["Profesi"]}");
+  // Menampilkan biodata
+  print("Nama: $nama");
+  print("Umur: $umur tahun");
+  print("Tinggi Badan: $tinggiBadan cm");
+  print("Status Aktif: $statusAktif");
+  print("Buku Favorit: $bukuFavorit");
+  print("Alamat: ${informasi["Alamat"]}");
+  print("Profesi: ${informasi["Profesi"]}");
 }

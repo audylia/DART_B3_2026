6d3b2e6d19010787ae96e7549de83b2ec16734f2
audy <audyliaaskawidiaputri@gmail.com>
2026-07-20 09:04:24 +0700

    Update tugas2.dart

--- tugas2.dart
+++ tugas2.dart
@@ -1,21 +1,16 @@
 void main() {
-  // Nilai mahasiswa
   int uts = 80;
   int uas = 75;
   double kehadiran = 90;
 
-  // Menghitung rata-rata
   double rataRata = (uts + uas) / 2;
 
-  // Mengecek syarat kelulusan
   bool syaratNilai = rataRata >= 70;
   bool syaratKehadiran = kehadiran >= 75;
   bool syaratMinimal = uts >= 60 && uas >= 60;
 
-  // Menggabungkan seluruh syarat
   bool lulus = syaratNilai && syaratKehadiran && syaratMinimal;
 
-  // Menampilkan hasil
   print("Nilai UTS: $uts");
   print("Nilai UAS: $uas");
   print("Kehadiran: $kehadiran%");
